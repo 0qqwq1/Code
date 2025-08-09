ae182318177b0051ddd5a099ee9f676b8f340e9c
0qqwq1 <plmi_123456@163.com>
2025-08-09 13:45:26 +0800

    完成了《CPP》的P114T1

---
+++ C/Ep114T1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define H 60
+
+int main(){
+ int m,h;
+ _Bool i=0;
+
+do{
+    printf("min:");
+    scanf("%d",&m);
+    if(m<=0){
+        i=0;break;
+    }
+    else{
+        i=1;
+    }
+    h=m/H;
+    m=m-H*h;
+    printf("%dh%dmin\n",h,m);
+}while (i=1);
+
+return 0;
+}
