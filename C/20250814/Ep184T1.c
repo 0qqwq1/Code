1164b6b0d78255518608be34de5c27cf76792108
0qqwq1 <plmi_123456@163.com>
2025-08-14 17:58:24 +0800

    将练习按日期分文件夹 完成了《cpp》P184T1编程练习

---
+++ C/20250814/Ep184T1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define SPACE ' '
+
+int main(){
+    int g=0,h=0,q=0;
+    char ch;
+    ch=getchar();
+
+    while(ch!='#') {
+        switch(ch){
+            case SPACE: g++;break;
+            case '\n' : h++;break;
+            default: q++;break;
+        }
+        ch=getchar();  
+        }
+    
+    printf("SPACE:%d\n",g);
+    printf("huanhan:%d\n",h);
+    printf("qita:%d\n",q);
+
+    return 0;
+}
