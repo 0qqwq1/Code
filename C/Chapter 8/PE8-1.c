e7433a8a88d6299287bdc0a6df008b547ae4bdf6
0qqwq1 <plmi_123456@163.com>
2025-12-25 18:40:12 +0800

    根据答案修改了第八章编程练习第一题

--- C/Chapter 8/PE8-1.c
+++ C/Chapter 8/PE8-1.c
@@ -5,7 +5,7 @@ int main(void)
     long i;
     while ((ch = getchar()) != EOF)
     {
-        if (ch != '\n')
+        if (ch > '\040')
             i++;
     }
     printf("There is a total of %d character in the file", i);
