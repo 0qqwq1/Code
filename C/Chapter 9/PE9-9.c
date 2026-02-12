35f93a8cf83f18ad7503620241041ef6e8ffd11e
0qqwq1 <plmi_123456@163.com>
2026-02-12 10:55:38 +0800

    完成了PE9-9

---
+++ C/Chapter 9/PE9-9.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+double power(double n, int p);
+
+int main(void)
+{
+    double x, xpow;
+    int exp, pow = 1;
+
+    printf("Enter a number and the positive integer power");
+    printf(" to which \nthe number wil be rasied. Enter q");
+    printf(" to quit.\n");
+    while (scanf("%lf%d", &x, &exp) == 2)
+    {
+        if (x != 0 || exp != 0){
+            xpow = power(x, exp);
+            printf("%.3g to the power %d is %.5g\n", x, exp, xpow);
+        }
+        else 
+            printf("Undefined! 0 to the power 0 is 1.\n");
+        printf("Enter next pair of numbers or q to quit.\n");
+    }
+    printf("Hope you enjoyed this power trip -- bye!\n");
+
+    return 0;
+}
+
+double power(double n, int p)
+{
+    double pow;
+
+    if (p > 0)
+        pow = n * power(n, p - 1);
+    else if (p < 0)
+        pow = (1 / n) * power(n, p + 1);
+    else 
+        pow = 1; 
+
+    if (n == 0)
+        return 0;
+
+    return pow;
+}
+
+/*
+double power(double n, int p)
+{
+    if (n == 0)
+        return 0;
+    
+    if (p == 0)
+        return 1;
+    
+    if (p > 0)
+        return n * power(n, p - 1);
+    else  // p < 0
+        return (1.0 / n) * power(n, p + 1);
+}
+*/
