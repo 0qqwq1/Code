eb29a1fe2a039d0dd9331d517da0aa152309ed36
0qqwq1 <plmi_123456@163.com>
2026-04-02 18:01:12 +0800

    完成了PE11-8

---
+++ C/Chapter 11/PE11-8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+char * string_in(const char * s1, const char * s2);
+int main(void)
+{
+    char * ptr;
+
+    if ((ptr = string_in("hats", "s")) == NULL)
+    puts("s2 is not contained within s1;");
+    else
+    printf("The address of s2 in s1 is %p", ptr);
+    return 0;
+
+}
+
+//找到s1字符串中s2字符串出现的首位置，若没有，则返回空指针。
+char * string_in(const char * s1, const char * s2)
+{
+    int i = 0;
+
+    while (strcmp(s1 + i, s2)) //一直循环，直到s1字符串中找到s2字符串为止。
+    {
+        if (*(s1 + i) == '\0') //需要退出循环的条件，可用strncmp()优化。
+            break;
+        i++;
+    }
+
+    if (strcmp(s2, "\0") && *(s1 + i) == '\0')
+        return NULL;
+    else 
+        return (char *)(s1 + i);
+}
