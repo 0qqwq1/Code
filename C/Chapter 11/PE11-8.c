eab76a5e81c1587cc6ee983a3f9c7c6547f0b850
0qqwq1 <plmi_123456@163.com>
2026-04-03 17:11:56 +0800

    完成了PE11-8

--- C/Chapter 11/PE11-8.c
+++ C/Chapter 11/PE11-8.c
@@ -5,7 +5,7 @@ int main(void)
 {
     char * ptr;
 
-    if ((ptr = string_in("hats", "s")) == NULL)
+    if ((ptr = string_in("hats", "qqqqqq")) == NULL)
     puts("s2 is not contained within s1;");
     else
     printf("The address of s2 in s1 is %p", ptr);
@@ -13,6 +13,28 @@ int main(void)
 
 }
 
+char * string_in(const char * s1, const char * s2)
+{
+    int i = 0, j = 0;
+
+    while (*(s1 + i) != '\0' && *(s2 + j) != '\0')
+    {
+        if (*((s1 + i) + j) != *(s2 + j))
+        {
+            j = 0;
+            i++;
+        }
+        else
+            j++;
+    }
+    
+    if (*s2 != '\0' && *(s1 + i) == '\0')
+        return NULL;
+    else
+        return (char *)(s1 + i);
+}
+ 
+/*
 //找到s1字符串中s2字符串出现的首位置，若没有，则返回空指针。
 char * string_in(const char * s1, const char * s2)
 {
@@ -30,3 +52,4 @@ char * string_in(const char * s1, const char * s2)
     else 
         return (char *)(s1 + i);
 }
+*/
